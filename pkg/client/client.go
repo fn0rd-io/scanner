cdd2830542fa605aa90ab8dccb75fb63494ebf7d
Daniel 'f0o' Preussker <f0o@devilcode.org>
2025-03-18 10:57:15 +0000

    Revert custom DialContext due to BSD oddities

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -125,18 +125,18 @@ func (c *Client) startWorkers() {
 func (c *Client) connectionManager() {
 	slog.Info(fmt.Sprintf("Connection manager started for coordinator at %s", c.config.CoordinatorURL))
 
-	transport := http.DefaultTransport
-	transport.(*http.Transport).DialContext = (&net.Dialer{
-		Timeout:   c.config.ConnectTimeout,
-		KeepAlive: 30 * time.Second,
-		DualStack: true,
-	}).DialContext
-	httpClient := http.DefaultClient
-	httpClient.Transport = transport
+	// transport := http.DefaultTransport
+	// transport.(*http.Transport).DialContext = (&net.Dialer{
+	// 	Timeout:   c.config.ConnectTimeout,
+	// 	KeepAlive: 30 * time.Second,
+	// 	DualStack: true,
+	// }).DialContext
+	// httpClient := http.DefaultClient
+	// httpClient.Transport = transport
 
 	// Create connect client with custom HTTP client
 	client := coordinatorconnect.NewCoordinatorServiceClient(
-		httpClient,
+		http.DefaultClient,
 		c.config.CoordinatorURL,
 		connect.WithGRPC(),
 	)
